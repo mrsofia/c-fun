7d42f2ddedb9ff2c6a6c058071cd377c7fef5f76
Justin Sofia <justin.sofia@bba12.mccombs.utexas.edu>
2015-12-24 17:54:29 -0600

    adds simple script to automate population of database with test vals

---
+++ populate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./ex17 db.dat c
+./ex17 db.dat s 1 "justin sofia" "justin.sofia@gmail.com"
+./ex17 db.dat s 2 "amber muhammad" "amber.m@gmail.com"
+./ex17 db.dat s 3 "shari hanks" "shanks@gmail.com"
+./ex17 db.dat s 4 "ron hanks" "rhanks@gmail.com"
+./ex17 db.dat s 5 "erika sofia" "ensofia@gmail.com"
+./ex17 db.dat s 6 "david sofia" "davidsofia@gmail.com"
+./ex17 db.dat l
